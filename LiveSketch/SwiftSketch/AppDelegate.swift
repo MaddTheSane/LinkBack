2d61e86f4ca96e864f0af94a9f99980223428408
C.W. Betts <computers57@hotmail.com>
2018-11-12 16:25:25 -0700

    Initial work on porting the Sketch app to Swift.

---
+++ LiveSketch/SwiftSketch/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  SwiftSketch
+//
+//  Created by C.W. Betts on 11/12/18.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+	func applicationDidFinishLaunching(_ aNotification: Notification) {
+		// Insert code here to initialize your application
+	}
+
+	func applicationWillTerminate(_ aNotification: Notification) {
+		// Insert code here to tear down your application
+	}
+
+
+}
+
